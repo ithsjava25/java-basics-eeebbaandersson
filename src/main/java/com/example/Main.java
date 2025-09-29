4e756114f97e51b7f99f8afb4b2a9fe3dcafbe41
Ebba Andersson <eeebbaandersson@gmail.com>
2025-09-29 19:30:40 +0200

    Skrev ut sortad lista, dock med 48 värden

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,8 +1,6 @@
 package com.example;
 
 import com.example.api.ElpriserAPI;
-
-import java.lang.classfile.FieldElement;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.time.LocalDate;
@@ -13,6 +11,8 @@ import java.time.format.DateTimeFormatter;
 public class Main {
     public static void main(String[] args) {
         ElpriserAPI elpriserAPI = new ElpriserAPI();
+        Locale.setDefault(new Locale("sv","SE"));
+
 
         //Minnesanteckningar för egen skull
         // --zone SE1|SE2|SE3|SE4 (required)
@@ -28,14 +28,12 @@ public class Main {
 
         System.out.println("--Välkommen till Elpriskollen--");
 
-
-        //Om inga argument matas in, ge felmeddelande och visa hjälpinfo
+        //Om inga argument matas in, ge felmeddelande/visa hjälpinfo
         if (args.length == 0) {
             System.out.println("Argument saknas.");
             printHelp();
             return;
         }
-
 
         //Loopa genom String argumenten som matas is
         for (int i = 0; i < args.length; i++) {
@@ -78,7 +76,6 @@ public class Main {
         //Validera zone
         List<String> validZones = Arrays.asList("SE1", "SE2", "SE3", "SE4");
 
-        //Todo: se över to.UperCase inmatningen i zonvalideringen?
         if (zone == null || !validZones.contains(zone.toUpperCase())) {
             System.out.println("Ogiltig zone. Välj någon av följande: SE1, SE2, SE3, SE4");
             return;
@@ -89,8 +86,6 @@ public class Main {
 
         //Validera date
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-
-        //Variabel för datum som LocalDate
         LocalDate parsedDate;
 
         if (date == null) {
@@ -100,7 +95,7 @@ public class Main {
             System.out.println("Inget datum angavs. Använder dagens datum. " + date);
         } else {
             try {
-               parsedDate = LocalDate.parse(date,formatter);
+                parsedDate = LocalDate.parse(date, formatter);
                 System.out.println("Valt datum: " + parsedDate);
             } catch (DateTimeParseException e) {
                 System.out.println("Ogiltigt datum, använd formatet (yyyy-MM-dd).");
@@ -108,8 +103,21 @@ public class Main {
             }
         }
 
-        //Anropar API:n med data/input från användaren
-        List<ElpriserAPI.Elpris> elpriser = elpriserAPI.getPriser(date, ElpriserAPI.Prisklass.valueOf(zone));
+        LocalDate nextDay = parsedDate.plusDays(1);
+        String currentDateStr = parsedDate.format(formatter);
+        String nextDateStr = nextDay.format(formatter);
+
+        //Skapar ny arraylist innehållandes dagens+morgondagens priser
+        List<ElpriserAPI.Elpris> elpriser = new ArrayList<>();
+
+        List<ElpriserAPI.Elpris> priserDag1 = elpriserAPI.getPriser(currentDateStr,
+                ElpriserAPI.Prisklass.valueOf(zone));
+        List<ElpriserAPI.Elpris> priserDag2 = elpriserAPI.getPriser(nextDateStr,
+                ElpriserAPI.Prisklass.valueOf(zone));
+
+        elpriser.addAll(priserDag1);
+        elpriser.addAll(priserDag2);
+
 
         if (!sorted) {
             try {
@@ -121,7 +129,6 @@ public class Main {
                 double averagePrice = getAveragePrice(elpriser);
 
                 DateTimeFormatter hourFormatter = DateTimeFormatter.ofPattern("HH");
-
                 String maxTime = maxPrice.timeStart().toLocalTime().format(hourFormatter) + "-"
                         + maxPrice.timeEnd().toLocalTime().format(hourFormatter);
 
@@ -147,11 +154,12 @@ public class Main {
                 System.out.println("Fel: Ogiltig laddningstid. Använd 2h, 4h eller 8h.");
                 return;
             }
-            //Lägg till metod/logik för att hitta billigaste laddningstimmen
+            //Todo: Lägg till metod/logik för att hitta billigaste laddningstimmen
             System.out.println("Vald laddningstid: " + charging);
         }
     }
 
+    // skilj på dagens datum/morgondagen
     //Metoder
     public static void validateElpriserList(List<ElpriserAPI.Elpris> elpriser) {
         if (elpriser == null || elpriser.isEmpty()) {
@@ -161,6 +169,7 @@ public class Main {
 
     public static void sortPrices(List<ElpriserAPI.Elpris> elpriser) {
         List<ElpriserAPI.Elpris> sorteradePriser = new ArrayList<>(elpriser);
+
         sorteradePriser.sort(Comparator.comparingDouble(ElpriserAPI.Elpris::sekPerKWh).reversed());
 
         DateTimeFormatter hourFormatter = DateTimeFormatter.ofPattern("HH");
@@ -199,12 +208,12 @@ public class Main {
         return min;
     }
 
-    public static double getAveragePrice(List<ElpriserAPI.Elpris> elpriser){
+    public static double getAveragePrice(List<ElpriserAPI.Elpris> elpriser) {
         double sum = 0.0;
-        for(ElpriserAPI.Elpris elpris : elpriser){
+        for (ElpriserAPI.Elpris elpris : elpriser) {
             sum += elpris.sekPerKWh();
         }
-        return sum/ elpriser.size();
+        return sum / elpriser.size();
     }
 
     public static void printHelp() {
